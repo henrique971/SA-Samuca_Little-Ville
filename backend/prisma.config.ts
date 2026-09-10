cb6c61ccac11e0a17e793f09f75fb4c2dd3996c6
antoniokauetesta <antoniokauetestasantos@gmail.com>
2026-09-10 17:03:12 -0300

    test

--- backend/prisma.config.ts
+++
@@ -1,8 +0,0 @@
-import { definePrismaConfig } from "prisma/config";
-
-export default definePrismaConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-});
